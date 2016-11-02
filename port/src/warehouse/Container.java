75aac95d365abb60be14159064e630f5757589d2
yanzharov <yanzharov@tut.by>
2016-11-02 12:23:03 +0300

    Update Container.java

--- port/src/warehouse/Container.java
+++ port/src/warehouse/Container.java
@@ -1,5 +1,5 @@
 package warehouse;
-
+/* Старая версия:
 public class Container {
     private int id;
 	
@@ -10,4 +10,53 @@ public class Container {
     public int getId(){
 	return id;
     }
+}
+Container-бин, который мы засовываем в коллекции. Поэтому нужно переопределить equals,hashCode,toString, имплементировать Serializable
+и объявить дефолтный конструктор
+*/
+public class Container implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private int id;
+
+    public Container() {
+    }
+	    
+    public Container(int id){
+	this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Container other = (Container) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Container{" + "id=" + id + '}';
+    }
+    
 }
